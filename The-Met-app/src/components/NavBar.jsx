142c3a25f1fd23fb9feeb461cf0ed7a578d36c31
darren lackie <lackiewwpj@gmail.com>
2023-10-08 15:49:36 +0100

    small css change

--- The-Met-app/src/components/NavBar.jsx
+++ The-Met-app/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
+                <li><a href="/">About</a></li>
             </ul>
         </nav>
     );
